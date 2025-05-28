f8b5e25d80dcc3535955cb379be8dce22fc83a7f
Oleh Tatarynov <exocriador@gmail.com>
2025-05-28 18:43:36 +0300

    refactor, add POST/PUT/PATCH/DELETE

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,37 @@
 import { Contacts } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
-    const contacts = await Contacts.find();
-    return contacts;
+  const contacts = await Contacts.find();
+  return contacts;
 };
 
 export const getContactById = async (contactId) => {
-    const contact = await Contacts.findById(contactId);
-    return contact;
+  const contact = await Contacts.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (contactData) => {
+  const newContact = await Contacts.create(contactData);
+  return newContact;
+};
+
+export const deleteContact = async (contactId) => {
+  const deletedContact = await Contacts.findByIdAndDelete(contactId);
+  return deletedContact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const { upsert = false } = options;
+  const rawResult = await Contacts.findByIdAndUpdate(contactId, payload, {
+    new: true,
+    upsert,
+    includeResultMetadata: true,
+  });
+  if (!rawResult || !rawResult.value) {
+    return null;
+  }
+  return {
+    data: rawResult.value,
+    isNew: Boolean(rawResult.lastErrorObject.upserted),
+  };
 };
