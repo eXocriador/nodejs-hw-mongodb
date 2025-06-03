11bc1a0408adac413f982a8fb5a3975aac15e88c
Oleh Tatarynov <exocriador@gmail.com>
2025-06-03 17:11:20 +0300

    fix / route

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import welcomeRoute from './routes/welcomeRoute.js';
+import welcomeRoute from './routers/welcomeRoute.js';
 import contactsRouter from './routers/contacts.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
