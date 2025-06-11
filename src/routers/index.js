304be96657f106c4e09dc8d324756713c2c6db2c
Oleh Tatarynov <exocriador@gmail.com>
2025-06-11 16:33:12 +0300

    add pu.sh, update startLogs

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,14 @@
 import { Router } from 'express';
+import welcomeRoute from './welcomeRoute.js';
+import contactsRouter from './contacts.js';
+import authRouter from './auth.js';
+import healthRouter from './health.js';
 
 const router = Router();
 
-router.use();
+router.use('/', welcomeRoute);
+router.use('/auth', authRouter);
+router.use('/contacts', contactsRouter);
+router.use('/health', healthRouter);
+
+export default router;
