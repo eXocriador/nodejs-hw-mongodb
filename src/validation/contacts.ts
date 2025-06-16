94af0427fc6c5d6c05a117952a29aa97d725745e
Oleh Tatarynov <exocriador@gmail.com>
2025-06-16 13:25:57 +0300

    refine, fix

--- src/validation/contacts.ts
+++ src/validation/contacts.ts
@@ -1,11 +1,13 @@
 import Joi from 'joi';
 import { ContactRequest, UpdateContactRequest } from '../types/models';
+import { typeList } from '../constants/contacts';
 
 export const contactSchema = Joi.object<ContactRequest>({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid(...typeList).required(),
 });
 
 export const updateContactSchema = Joi.object<UpdateContactRequest>({
@@ -13,4 +15,5 @@ export const updateContactSchema = Joi.object<UpdateContactRequest>({
   email: Joi.string().email(),
   phoneNumber: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid(...typeList),
 }).min(1);
